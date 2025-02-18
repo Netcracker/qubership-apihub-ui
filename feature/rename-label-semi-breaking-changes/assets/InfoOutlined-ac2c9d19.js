71dda1837713a54a3371da5e5900a3f0ed85b599
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2025-02-18 15:26:34 +0000

    deploy: b49d7d936d7d849b9f9e5f11c219a3e672c3bfdc

---
+++ feature/rename-label-semi-breaking-changes/assets/InfoOutlined-ac2c9d19.js
@@ -0,0 +1,2 @@
+import{r,i as a,a as t}from"./jsx-runtime_commonjs-proxy-c1eabaee.js";var e={},u=a;Object.defineProperty(e,"__esModule",{value:!0});var i=e.default=void 0,o=u(r()),d=t,v=(0,o.default)((0,d.jsx)("path",{d:"M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"}),"InfoOutlined");i=e.default=v;export{i as d};
+//# sourceMappingURL=InfoOutlined-ac2c9d19.js.map
---
+++ feature/rename-label-semi-breaking-changes/assets/decorators-8b7a0c6a.js
@@ -0,0 +1,2 @@
+import{j as t}from"./createTheme-0ee58ceb.js";function h(e,i){return t("div",{style:{height:"100vh"},children:t(e,{...i})})}export{h as f};
+//# sourceMappingURL=decorators-8b7a0c6a.js.map
