228d981d38f74fc5fae55b28b32ec31d16366420
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-03-06 18:42:12 +0400

    Implemented e2e approach to handle failed fetching of dynamically imported module

---
+++ packages/shared/src/components/ModuleFetchingErrorBoundary/ModuleFetchingErrorDialog.tsx
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2024-2025 NetCracker Technology Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Autorenew } from '@mui/icons-material'
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material'
+import type { FC, PropsWithChildren } from 'react'
+
+export const ModuleFetchingErrorPopup: FC<PropsWithChildren> = () => (
+  <Dialog
+    open
+    PaperProps={{
+      sx: {
+        borderRadius: '10px',
+      },
+    }}
+  >
+    <DialogTitle textAlign='center'>
+      APIHUB is out of date
+    </DialogTitle>
+    <DialogContent>
+      <Typography variant='body2' textAlign='center'>
+        The version of APIHUB is out of date.
+        Please reload the page to get the latest version;
+        otherwise, APIHUB may not function correctly
+      </Typography>
+    </DialogContent>
+    <DialogActions
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Button
+        variant='outlined'
+        startIcon={<Autorenew />}
+        onClick={() => { location.reload() }}
+      >
+        Reload
+      </Button>
+    </DialogActions>
+  </Dialog>
+)
