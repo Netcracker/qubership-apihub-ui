dd856d6005f3fc440ef74b3e56b0ba01d208f1b3
b41ex <b41ex@users.noreply.github.com>
2025-03-21 13:39:54 +0000

    deploy: 9d1313901bc048e9dffba649a852be56bd33c472

---
+++ 2.10.1/assets/CustomListItemButton.stories-e1b1caab.js
@@ -0,0 +1,30 @@
+import{j as a}from"./createTheme-0ee58ceb.js";import{C,L as c,a as d}from"./CustomListItemButton-02e6e15c.js";import{G as p}from"./placeholder-colors-743b01e8.js";import{B as I}from"./Box-b4966c9a.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-dd1a14a8.js";import"./ListItemButton-3d5f83f7.js";import"./clsx.m-bc31e435.js";import"./styled-63e5ab44.js";import"./useThemeProps-af595d22.js";import"./ListContext-aeaca2aa.js";import"./listItemButtonClasses-ec8f8e5b.js";import"./ButtonBase-11c344bc.js";import"./emotion-react.browser.esm-e1f4c3b5.js";import"./hoist-non-react-statics.cjs-b1c88361.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-373e5cc0.js";import"./extendSxProp-083dfeeb.js";const P={title:"Custom List Item Button",component:C},t={args:{itemComponent:a(I,{sx:{backgroundColor:p},children:"Item Component of a Cat List"}),isSelected:!0,isSubListItem:!1,keyProp:"item-1",size:c,data:{id:"entity-1",name:"Cat"}}},e={args:{itemComponent:a(I,{sx:{backgroundColor:p},children:"Item Component of a Cat List"}),isSelected:!0,isSubListItem:!1,keyProp:"item-1",size:d,data:{id:"entity-1",name:"Cat"}}};var o,r,m;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    itemComponent: <Box sx={{
+      backgroundColor: GREEN_SECTION_COLOR
+    }}>Item Component of a Cat List</Box>,
+    isSelected: true,
+    isSubListItem: false,
+    keyProp: 'item-1',
+    size: LIST_ITEM_SIZE_SMALL,
+    data: {
+      id: 'entity-1',
+      name: 'Cat'
+    }
+  }
+}`,...(m=(r=t.parameters)==null?void 0:r.docs)==null?void 0:m.source}}};var n,i,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    itemComponent: <Box sx={{
+      backgroundColor: GREEN_SECTION_COLOR
+    }}>Item Component of a Cat List</Box>,
+    isSelected: true,
+    isSubListItem: false,
+    keyProp: 'item-1',
+    size: LIST_ITEM_SIZE_BIG,
+    data: {
+      id: 'entity-1',
+      name: 'Cat'
+    }
+  }
+}`,...(s=(i=e.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const Z=["SmallListItemButton","BigListItemButton"];export{e as BigListItemButton,t as SmallListItemButton,Z as __namedExportsOrder,P as default};
+//# sourceMappingURL=CustomListItemButton.stories-e1b1caab.js.map
