22ef79ec228723356cd1dd75fd2842c3d36328c8
b41ex <b41ex@users.noreply.github.com>
2025-03-22 18:28:29 +0000

    deploy: 44fea579feafe526739c9b407c0c5e9d21adef5d

---
+++ feature/enum-changes-risky/assets/emotion-use-insertion-effect-with-fallbacks.browser.esm-dd1a14a8.js
@@ -0,0 +1,2 @@
+import{r as c,e as r}from"./index-37ba2b57.js";function s(){return s=Object.assign?Object.assign.bind():function(a){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var e in n)Object.prototype.hasOwnProperty.call(n,e)&&(a[e]=n[e])}return a},s.apply(this,arguments)}var o=function(t){return t()},f=r["useInsertionEffect"]?r["useInsertionEffect"]:!1,i=f||o,l=f||c.useLayoutEffect;export{s as _,i as a,l as u};
+//# sourceMappingURL=emotion-use-insertion-effect-with-fallbacks.browser.esm-dd1a14a8.js.map
