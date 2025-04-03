280a00052de60b2adacdd4e425521ae453b54004
Iurii Golovinskii <iurii-golovinskii.qubership@gmail.com>
2025-04-03 17:45:59 +0300

    Rename label risky to change requiring attention

--- packages/shared/src/entities/change-severities.ts
+++ packages/shared/src/entities/change-severities.ts
@@ -62,7 +62,7 @@ export const CHANGE_SEVERITY_COLOR_MAP: Record<ChangeSeverity, string> = {
 
 export const CHANGE_SEVERITY_NAME_MAP: Record<ChangeSeverity, string> = {
   [breaking]: 'Breaking',
-  [risky]: 'Risky',
+  [risky]: 'Changes Requiring Attention',
   [deprecated]: 'Deprecated',
   [nonBreaking]: 'Non-breaking',
   [unclassified]: 'Unclassified',
@@ -72,7 +72,7 @@ export const CHANGE_SEVERITY_NAME_MAP: Record<ChangeSeverity, string> = {
 export const CHANGE_SEVERITY_DESCRIPTION_MAP: Record<ChangeSeverity, { text: string; options?: string[] }> = {
   [breaking]: { text: 'Breaking change is a change that breaks backward compatibility with the previous version of API. For example, deleting an operation, adding a required parameter or changing type of a parameter are breaking changes.' },
   [risky]: {
-    text: 'Risky change is a change that breaks backward compatibility according to the rules:',
+    text: 'A change requiring attention is a change that breaks backward compatibility according to the rules:',
     options: ['operation was annotated as deprecated in at least two previous consecutive releases and then it was deleted', 'operation is marked as no-BWC'],
   },
   [deprecated]: { text: 'Deprecating change is a change that annotates an operation, parameter or schema as deprecated. Removing a "deprecated" annotation is also considered a deprecating change.' },
