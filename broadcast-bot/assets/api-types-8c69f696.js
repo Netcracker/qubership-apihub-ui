b9f497b40a26c2f76866d1939fe30dc1e54d847a
NetcrackerCLPLCI <NetcrackerCLPLCI@users.noreply.github.com>
2025-04-09 10:29:02 +0000

    deploy: c2104c1fb613b012ea75ab4c140d1588354e645d

---
+++ broadcast-bot/assets/api-types-8c69f696.js
@@ -0,0 +1,2 @@
+import{R as _,G as T}from"./apihub-builder.es-d609de32.js";const P=_,A=T,E=[P,A],I={[P]:"REST API",[A]:"GraphQL API"};export{E as A,I as a,P as b,A as c};
+//# sourceMappingURL=api-types-8c69f696.js.map
