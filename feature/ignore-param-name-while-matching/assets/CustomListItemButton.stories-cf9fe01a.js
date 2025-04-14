4dd6a238957f509d7e1eb5a4b980b19fc172bfc6
CountRedClaw <CountRedClaw@users.noreply.github.com>
2025-04-14 16:30:22 +0000

    deploy: 2a6d4bbadce9caa7b1910fece19acfb98dab140c

---
+++ feature/ignore-param-name-while-matching/assets/CustomListItemButton.stories-cf9fe01a.js
@@ -0,0 +1,30 @@
+import{j as a}from"./createTheme-8731d8a8.js";import{C,L as c,a as d}from"./CustomListItemButton-e50547d7.js";import{G as p}from"./placeholder-colors-743b01e8.js";import{B as I}from"./Box-88903741.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./ListItemButton-f9732a13.js";import"./clsx.m-5e43a548.js";import"./index-0cc5c49e.js";import"./useThemeProps-5ce1434c.js";import"./ListContext-aeaca2aa.js";import"./listItemButtonClasses-53232159.js";import"./ButtonBase-4f5f2753.js";import"./emotion-react.browser.esm-3c20310b.js";import"./hoist-non-react-statics.cjs-b1c88361.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-5494d9cc.js";import"./TransitionGroupContext-0440df69.js";import"./extendSxProp-033d7d5d.js";const P={title:"Custom List Item Button",component:C},t={args:{itemComponent:a(I,{sx:{backgroundColor:p},children:"Item Component of a Cat List"}),isSelected:!0,isSubListItem:!1,keyProp:"item-1",size:c,data:{id:"entity-1",name:"Cat"}}},e={args:{itemComponent:a(I,{sx:{backgroundColor:p},children:"Item Component of a Cat List"}),isSelected:!0,isSubListItem:!1,keyProp:"item-1",size:d,data:{id:"entity-1",name:"Cat"}}};var o,r,m;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    itemComponent: <Box sx={{
+      backgroundColor: GREEN_SECTION_COLOR
+    }}>Item Component of a Cat List</Box>,
+    isSelected: true,
+    isSubListItem: false,
+    keyProp: 'item-1',
+    size: LIST_ITEM_SIZE_SMALL,
+    data: {
+      id: 'entity-1',
+      name: 'Cat'
+    }
+  }
+}`,...(m=(r=t.parameters)==null?void 0:r.docs)==null?void 0:m.source}}};var n,i,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    itemComponent: <Box sx={{
+      backgroundColor: GREEN_SECTION_COLOR
+    }}>Item Component of a Cat List</Box>,
+    isSelected: true,
+    isSubListItem: false,
+    keyProp: 'item-1',
+    size: LIST_ITEM_SIZE_BIG,
+    data: {
+      id: 'entity-1',
+      name: 'Cat'
+    }
+  }
+}`,...(s=(i=e.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};const Z=["SmallListItemButton","BigListItemButton"];export{e as BigListItemButton,t as SmallListItemButton,Z as __namedExportsOrder,P as default};
+//# sourceMappingURL=CustomListItemButton.stories-cf9fe01a.js.map
---
+++ feature/ignore-param-name-while-matching/assets/listItemTextClasses-08ea4e3c.js
@@ -0,0 +1,2 @@
+import{g as e,b as s}from"./clsx.m-5e43a548.js";function l(t){return s("MuiListItemText",t)}const i=e("MuiListItemText",["root","multiline","dense","inset","primary","secondary"]),n=i;export{l as g,n as l};
+//# sourceMappingURL=listItemTextClasses-08ea4e3c.js.map
