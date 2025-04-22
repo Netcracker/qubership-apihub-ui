ce64949868647cd60ee8c319ffa7ccee73ec5d4a
divy-netcracker <divy-netcracker@users.noreply.github.com>
2025-04-22 16:32:21 +0000

    deploy: 399553f66dff8aa125b0a13414872b7294312819

---
+++ bugfix/missing-prefix-value/assets/listItemIconClasses-c483526b.js
@@ -0,0 +1,2 @@
+import{g as s,a as e}from"./styled-63e5ab44.js";function l(t){return e("MuiListItemIcon",t)}const i=s("MuiListItemIcon",["root","alignItemsFlexStart"]),n=i;export{l as g,n as l};
+//# sourceMappingURL=listItemIconClasses-c483526b.js.map
