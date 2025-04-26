d949c3cd27408707dc85df3b5a31029466ea1a42
vOrigins <vOrigins@users.noreply.github.com>
2025-04-26 12:40:01 +0000

    deploy: 9b3c1c5a14e1104080405fe7201484953ef43adc

---
+++ feature/code-style-super-linter-ci/assets/api-types-ebd75f80.js
@@ -0,0 +1,2 @@
+import{R as _,G as T}from"./apihub-builder.es-d8025437.js";const P=_,A=T,E=[P,A],I={[P]:"REST API",[A]:"GraphQL API"};export{E as A,I as a,P as b,A as c};
+//# sourceMappingURL=api-types-ebd75f80.js.map
