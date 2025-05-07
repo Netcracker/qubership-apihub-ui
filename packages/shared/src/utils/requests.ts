f169e5e4a7a6a4ca4a0a7eb39ddfe33296a7c9d7
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-05-07 13:00:56 +0400

    feat(new auth): Restored absent query param "redirectUri". Added auto-redirecting on HTTP status 401 on refreshing token

--- packages/shared/src/utils/requests.ts
+++ packages/shared/src/utils/requests.ts
@@ -18,8 +18,9 @@ import type { IdentityProviderDto, SystemConfigurationDto } from '../types/syste
 import { isInternalIdentityProvider } from '../types/system-configuration'
 import { SEARCH_PARAM_NO_AUTO_LOGIN, SESSION_STORAGE_KEY_LAST_IDENTITY_PROVIDER_ID, SESSION_STORAGE_KEY_SYSTEM_CONFIGURATION } from './constants'
 import type { ErrorMessage } from './packages-builder'
-import { redirectToLogin } from './redirects'
+import { redirectTo, redirectToLogin } from './redirects'
 import { HttpError } from './responses'
+import { optionalSearchParams } from './search-params'
 import type { Key } from './types'
 
 export const API_V1 = '/api/v1'
@@ -188,18 +189,28 @@ async function handleAuthentication(response: Response): Promise<void> {
 
 async function handleUnauthorizedByProvider(
   identityProvider: IdentityProviderDto | undefined,
+  enforceRedirect: boolean = false,
 ): Promise<void> {
-  if (identityProvider) {
-    if (isInternalIdentityProvider(identityProvider)) {
-      redirectToLogin()
-    } else if (identityProvider.loginStartEndpoint) {
-      await fetch(
-        identityProvider.loginStartEndpoint,
-        {
-          credentials: 'include',
-          method: 'GET',
-        },
-      )
+  if (!identityProvider) {
+    return
+  }
+  if (isInternalIdentityProvider(identityProvider)) {
+    enforceRedirect && redirectToLogin()
+    const response = await fetch(
+      `${API_V3}/auth/local/refresh`,
+      { method: 'GET' },
+    )
+    if (response.status === 401) {
+      handleUnauthorizedByProvider(identityProvider, true)
+    }
+  } else if (identityProvider.loginStartEndpoint) {
+    enforceRedirect && redirectTo(identityProvider.loginStartEndpoint)
+    const response = await fetch(
+      `${identityProvider.loginStartEndpoint}?${optionalSearchParams({ redirectUri: { value: location.href } })}`,
+      { method: 'GET' },
+    )
+    if (response.status === 401) {
+      handleUnauthorizedByProvider(identityProvider, true)
     }
   }
 }
