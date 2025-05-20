859b8d66205a0fa5878cca8c4bbd3aa0d7777c78
NetcrackerCLPLCI <NetcrackerCLPLCI@users.noreply.github.com>
2025-05-20 08:40:21 +0000

    deploy: 220c5257ac1b922a92704ec910e45b398bbbc051

---
+++ broadcast-config-files/assets/listItemTextClasses-3dd10bc0.js
@@ -0,0 +1,2 @@
+import{g as e,b as s}from"./clsx.m-e8952646.js";function l(t){return s("MuiListItemText",t)}const i=e("MuiListItemText",["root","multiline","dense","inset","primary","secondary"]),n=i;export{l as g,n as l};
+//# sourceMappingURL=listItemTextClasses-3dd10bc0.js.map
