b0a413d44d9dafbc4dd47e21a77d9514486da700
b41ex <b41ex@users.noreply.github.com>
2025-05-27 10:35:21 +0000

    deploy: 13856eac3ca8dc07a75679994e6f44b29ebe5bce

---
+++ feature/export-build-tasks-support/assets/api-types-95d252ab.js
@@ -0,0 +1,2 @@
+import{R as _,G as T}from"./apihub-builder.es-bf0e6a1e.js";const P=_,A=T,E=[P,A],I={[P]:"REST API",[A]:"GraphQL API"};export{E as A,I as a,P as b,A as c};
+//# sourceMappingURL=api-types-95d252ab.js.map
