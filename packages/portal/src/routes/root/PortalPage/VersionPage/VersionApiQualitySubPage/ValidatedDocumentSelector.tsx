b9e30c4b171516c4a6fec899ec3ffeb90218f361
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-06-24 10:53:28 +0400

    feat(linter): Updated layout for ValidatedDocumentSelector based on mocked values

--- packages/portal/src/routes/root/PortalPage/VersionPage/VersionApiQualitySubPage/ValidatedDocumentSelector.tsx
+++ packages/portal/src/routes/root/PortalPage/VersionPage/VersionApiQualitySubPage/ValidatedDocumentSelector.tsx
@@ -1,12 +1,37 @@
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined'
-import { Box, Button } from '@mui/material'
+import { Box, Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { MenuButtonItems } from '@netcracker/qubership-apihub-ui-shared/components/Buttons/MenuButton'
+import { SearchBar } from '@netcracker/qubership-apihub-ui-shared/components/SearchBar'
 import { SpecLogo } from '@netcracker/qubership-apihub-ui-shared/components/SpecLogo'
 import { TextWithOverflowTooltip } from '@netcracker/qubership-apihub-ui-shared/components/TextWithOverflowTooltip'
-import { useState, type FC } from 'react'
+import { useMemo, useState, type FC } from 'react'
+
+function randomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 export const ValidatedDocumentSelector: FC = () => {
   const [anchor, setAnchor] = useState<HTMLElement>()
+
+  const [searchValue, setSearchValue] = useState('')
+
+  const list = useMemo(() => {
+    const length = randomInt(10, 15)
+    const originalList = Array(length).fill(0).map((_, index) => ({
+      name: `Document ${index + 1}`,
+      type: 'openapi-3-0',
+    }))
+    if (searchValue) {
+      return originalList.filter((document) => {
+        const docName = document.name.toLowerCase()
+        const docNameSubstring = searchValue.toLowerCase()
+        return docName.includes(docNameSubstring)
+      })
+    }
+    return originalList
+  }, [searchValue])
+
+  const [selectedDocument, setSelectedDocument] = useState(list[0])
 
   return (
     <Button
@@ -23,13 +48,13 @@ export const ValidatedDocumentSelector: FC = () => {
       endIcon={<KeyboardArrowDownOutlinedIcon />}
     >
       <Box display='flex' alignItems='flex-start' gap={1}>
-        <SpecLogo value={'openapi-3-0'} />
+        <SpecLogo value={selectedDocument.type} />
         <TextWithOverflowTooltip
-          tooltipText="Document"
+          tooltipText={selectedDocument.name}
           variant="body1"
           sx={{ fontWeight: 500 }}
         >
-          Document Name
+          {selectedDocument.name}
         </TextWithOverflowTooltip>
       </Box>
       <MenuButtonItems
@@ -39,9 +64,39 @@ export const ValidatedDocumentSelector: FC = () => {
         onClose={() => setAnchor(undefined)}
       >
         <Box
-          sx={{ p: 2, pt: 0, width: 300, height: 200 }}
+          sx={{ p: 1, width: 300, height: 216, pb: '36px', overflow: 'hidden' }}
         >
-          Text
+          <SearchBar onValueChange={setSearchValue} data-testid="ValidatedDocumentSearchBar" />
+          <List sx={{ overflowX: 'hidden', overflowY: 'auto' }}>
+            {list.map((document) => (
+              <ListItem key={document.name} sx={{ p: 0 }}>
+                <ListItemButton
+                  sx={{
+                    flexDirection: 'unset',
+                    backgroundColor: document.name === selectedDocument.name ? '#ECEDEF' : 'transparent',
+                    height: '36px',
+                    alignItems: 'center',
+                    '&:hover': {
+                      '& .MuiButtonBase-root': {
+                        visibility: 'visible',
+                      },
+                    },
+                  }}
+                  selected={document.name === selectedDocument.name}
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    setSelectedDocument(document)
+                  }}
+                  data-testid="ValidatedDocumentButton"
+                >
+                  <ListItemIcon sx={{ minWidth: 2, mt: 0, mr: 1 }}>
+                    <SpecLogo value={document.type} />
+                  </ListItemIcon>
+                  <ListItemText primary={document.name} primaryTypographyProps={{ sx: { mt: 0.25 } }} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
         </Box>
       </MenuButtonItems>
     </Button>
