95db6e64065329fce369bd48d72ff905bcba7166
Divy Tripathy <divy.tripathy@netcracker.com>
2025-06-24 14:04:49 +0530

    feat: added new reusable component

---
+++ packages/portal/src/entities/usePathWarning.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import { debounce } from 'lodash'
+
+export function usePathWarning(url: string, delay = 700): boolean {
+    const [showWarning, setShowWarning] = useState(false)
+
+    useEffect(() => {
+        if (!url) {
+            setShowWarning(false)
+            return
+        }
+
+        const handler = debounce(() => {
+            try {
+                const { pathname } = new URL(url)
+                const hasPath = pathname === '' || pathname === '/'
+                setShowWarning(hasPath)
+            } catch {
+                setShowWarning(false)
+            }
+        }, delay)
+
+        handler()
+
+        return () => {
+            handler.cancel()
+        }
+    }, [url, delay])
+
+    return showWarning
+}
+
