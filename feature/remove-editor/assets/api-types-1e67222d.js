2ded21caf890aa6cb8ae805238627bb3385a437c
b41ex <b41ex@users.noreply.github.com>
2025-07-01 12:29:05 +0000

    deploy: 3efe0af1323f0ff70d4e29bbc36d955cd4bd40ec

--- feature/remove-editor/assets/api-types-1e67222d.js
+++
@@ -1,2 +0,0 @@
-import{R as _,G as T}from"./apihub-builder.es-226d0bee.js";const P=_,A=T,E=[P,A],I={[P]:"REST API",[A]:"GraphQL API"};export{E as A,I as a,P as b,A as c};
-//# sourceMappingURL=api-types-1e67222d.js.map
---
+++ feature/remove-editor/assets/api-types-b4978761.js
@@ -0,0 +1,2 @@
+import{R as _,G as T}from"./apihub-builder.es-4babc84d.js";const P=_,A=T,E=[P,A],I={[P]:"REST API",[A]:"GraphQL API"};export{E as A,I as a,P as b,A as c};
+//# sourceMappingURL=api-types-b4978761.js.map
