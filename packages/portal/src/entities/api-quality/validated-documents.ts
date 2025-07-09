d94b0d1a30c431e2764816242589546c22c1ec3b
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-07-09 18:11:29 +0400

    feat(linter): Little fix types after refactoring

--- packages/portal/src/entities/api-quality/validated-documents.ts
+++ packages/portal/src/entities/api-quality/validated-documents.ts
@@ -1,7 +1,9 @@
+import type { Key } from '../keys'
+
 export type ValidatedDocumentDto = {
   documentId: string
   specificationType: 'openapi-2-0' | 'openapi-3-0' | 'openapi-3-1'
   title: string
 }
 
-export type ValidatedDocument = ValidatedDocumentDto
+export type ValidatedDocument = Omit<ValidatedDocumentDto, 'documentId'> & { id: Key }
