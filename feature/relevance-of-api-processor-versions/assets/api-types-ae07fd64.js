1150c6be378b3c5f3cbd855957cca363fa6f6710
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2025-07-11 11:16:32 +0000

    deploy: 4d6adc0e32c69e5e74ff617c56ae18a42d678b59

--- feature/relevance-of-api-processor-versions/assets/api-types-ae07fd64.js
+++
@@ -1,2 +0,0 @@
-import{R as _,G as T}from"./apihub-builder.es-ef2bfb55.js";const P=_,A=T,E=[P,A],I={[P]:"REST API",[A]:"GraphQL API"};export{E as A,I as a,P as b,A as c};
-//# sourceMappingURL=api-types-ae07fd64.js.map
