626588e48d2557596c01878734f289948d00fbbd
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-07-14 18:12:34 +0400

    feat(linter): Improved alignment of Validation Ruleset Link

--- packages/portal/src/components/ApiQuality/ValidatationRulesetLink.tsx
+++ packages/portal/src/components/ApiQuality/ValidatationRulesetLink.tsx
@@ -3,18 +3,18 @@ import { useEventBus } from '@apihub/routes/EventBusProvider'
 import { Box, Link, Skeleton, Typography } from '@mui/material'
 import { CustomChip } from '@netcracker/qubership-apihub-ui-shared/components/CustomChip'
 import type { IsLoading } from '@netcracker/qubership-apihub-ui-shared/utils/aliases'
-import type { FC } from 'react'
+import type { FC, ReactElement } from 'react'
 import { memo } from 'react'
 
 type ValidationRulesetLinkProps = {
   data: RulesetBase | undefined
   loading: IsLoading
-  noLabel?: boolean
+  showLabel?: boolean
 }
 
 // First Order Component
 export const ValidationRulesettLink: FC<ValidationRulesetLinkProps> = memo<ValidationRulesetLinkProps>(props => {
-  const { data, loading, noLabel = false } = props
+  const { data, loading, showLabel = true } = props
 
   const { showRulesetInfoDialog } = useEventBus()
 
@@ -26,33 +26,49 @@ export const ValidationRulesettLink: FC<ValidationRulesetLinkProps> = memo<Valid
     return null
   }
 
-  let linkElement = (
-    <Typography variant='body2' onClick={() => showRulesetInfoDialog(data)}>
+  const elements: ReactElement[] = []
+
+  elements.push(
+    <Typography
+      key='validation-ruleset-link-name'
+      variant='body2'
+      onClick={() => showRulesetInfoDialog(data)}
+    >
       <Link>
         {data.name}
       </Link>
-    </Typography>
+    </Typography>,
   )
 
   if (data.status === RulesetStatuses.INACTIVE) {
-    linkElement = (
-      <Box display='flex' alignItems='center' gap={1}>
-        {linkElement}
-        <CustomChip value='secondary' sx={{ m: 0 }} label={data.status} />
-      </Box>
+    elements.push(
+      <CustomChip
+        key='validation-ruleset-link-status'
+        value='secondary'
+        sx={{ m: 0 }} label={data.status}
+      />,
     )
   }
 
-  if (noLabel) {
-    return linkElement
-  }
-
-  return <>
-    <Box display='flex' gap={1}>
-      <Typography variant='body2' component='span'>
+  if (showLabel) {
+    elements.splice(0, 0, (
+      <Typography
+        key='validation-ruleset-link-label'
+        variant='body2'
+        component='span'
+      >
         Validated using
       </Typography>
-      {linkElement}
+    ))
+  }
+
+  if (elements.length === 0) {
+    return elements[0]
+  }
+
+  return (
+    <Box display='flex' alignItems='center' gap={1}>
+      {elements}
     </Box>
-  </>
+  )
 })
--- packages/portal/src/routes/root/PortalPage/VersionPage/VersionOverviewSubPage/SummaryTab/OperationTypeSummary.tsx
+++ packages/portal/src/routes/root/PortalPage/VersionPage/VersionOverviewSubPage/SummaryTab/OperationTypeSummary.tsx
@@ -18,6 +18,7 @@ import { ValidationRulesettLink } from '@apihub/components/ApiQuality/Validatati
 import { ValidationIssuesTooltip } from '@apihub/components/ApiQuality/ValidationIssuesTooltip'
 import type { IssueSeverity } from '@apihub/entities/api-quality/issue-severities'
 import { ISSUE_SEVERITIES_LIST, ISSUE_SEVERITY_COLOR_MAP, IssueSeverities } from '@apihub/entities/api-quality/issue-severities'
+import { RulesetStatuses } from '@apihub/entities/api-quality/rulesets'
 import { Box, Tooltip, Typography } from '@mui/material'
 import { API_AUDIENCE_EXTERNAL, API_AUDIENCE_INTERNAL, API_AUDIENCE_UNKNOWN, type ApiAudienceTransition } from '@netcracker/qubership-apihub-api-processor'
 import { Changes } from '@netcracker/qubership-apihub-ui-shared/components/Changes'
@@ -72,6 +73,7 @@ export const OperationTypeSummary: FC<OperationTypeSummaryProps> = memo<Operatio
   const showApiQualitySummary = validationFailed || (!apiQualitySummaryPlaceholder && !apiQualitySummaryDisabled)
   const validationSummary = useApiQualityValidationSummary()
   const validationRuleset = validationSummary?.[0]?.ruleset
+  const isInactiveValidationRuleset = validationRuleset?.status === RulesetStatuses.INACTIVE
   const aggregatedValidationSummary: Record<IssueSeverity, number> = useMemo(() => {
     const emptyValidationSummary: Record<IssueSeverity, number> = {
       [IssueSeverities.ERROR]: 0,
@@ -316,12 +318,25 @@ export const OperationTypeSummary: FC<OperationTypeSummaryProps> = memo<Operatio
 
               {showApiQualitySummary && <>
                 {validationRuleset && <>
-                  <Typography sx={{ gridArea: 'validationRulesetTitle' }} variant="subtitle2">
+                  <Typography
+                    sx={{
+                      gridArea: 'validationRulesetTitle',
+                      ...(isInactiveValidationRuleset ? { display: 'flex', alignItems: 'center' } : {}),
+                    }}
+                    variant="subtitle2"
+                  >
                     Validation ruleset
                   </Typography>
 
-                  <Box sx={{ gridArea: 'validationRuleset' }}>
-                    <ValidationRulesettLink data={validationRuleset} loading={!validationRuleset} noLabel />
+                  <Box sx={{
+                    gridArea: 'validationRuleset',
+                    ...(isInactiveValidationRuleset ? { display: 'flex', alignItems: 'center' } : {}),
+                  }}>
+                    <ValidationRulesettLink
+                      data={validationRuleset}
+                      loading={!validationRuleset}
+                      showLabel={false}
+                    />
                   </Box>
                 </>}
 
