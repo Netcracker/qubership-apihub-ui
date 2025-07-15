ffad0bcc2f2fa6b37402e282be2128490a43f81b
b41ex <b41ex@users.noreply.github.com>
2025-07-15 17:02:10 +0000

    deploy: 5f6346a73e49517cbd7a15f7e4c09e5b6b17e015

---
+++ feature/support-oas-extension-changes-classification/assets/api-types-1473b29a.js
@@ -0,0 +1,2 @@
+import{R as _,G as T}from"./apihub-builder.es-81de2564.js";const P=_,A=T,E=[P,A],I={[P]:"REST API",[A]:"GraphQL API"};export{E as A,I as a,P as b,A as c};
+//# sourceMappingURL=api-types-1473b29a.js.map
