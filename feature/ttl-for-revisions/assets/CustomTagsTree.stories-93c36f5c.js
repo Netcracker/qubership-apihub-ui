a7ae2fb476ba1784197105d582ab904ca2d48f59
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2025-07-17 21:36:30 +0000

    deploy: 25535191115aed4af6802b7169647dbecd020b13

---
+++ feature/ttl-for-revisions/assets/CustomTagsTree.stories-93c36f5c.js
@@ -0,0 +1,7 @@
+import{j as e}from"./createTheme-e504d7a1.js";import{C as m}from"./CustomTagsTree-9b0685d1.js";import{B as n}from"./Box-3dbc4f20.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./Typography-74efa758.js";import"./clsx.m-e8952646.js";import"./index-65a2139a.js";import"./useThemeProps-be56d1e2.js";import"./extendSxProp-c6b156d7.js";const u={"x-custom1":"X-Custom-1 Value","x-custom2":["green",12,null,"undefined"],"x-custom3":[{prop:1},{prop:"undefined"},"text text"],"x-custom4":null,"x-custom5":"undefined","x-custom6":{aaa3:!1},"x-custom7":{one1:{two1:{three1:"text1"},two2:{three1:"text1",three2:{four1:{five1:"text1"},four2:123}},two3:"text1"}},"x-custom8":"test"},D={title:"Custom Tags Tree",component:m,decorators:[a=>e(n,{sx:{ul:{"list-style-type":"none","padding-inline-start":0}},children:e(a,{})})]},t={name:"Default",args:{tree:u}};var o,r,s;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    tree: tree
+  }
+}`,...(s=(r=t.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const T=["DefaultStory"];export{t as DefaultStory,T as __namedExportsOrder,D as default};
+//# sourceMappingURL=CustomTagsTree.stories-93c36f5c.js.map
