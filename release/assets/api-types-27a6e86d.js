406ea0cc186359beffb9993f0038e3913d10a43f
alagishev <alagishev@users.noreply.github.com>
2025-07-25 08:41:11 +0000

    deploy: 491cb33a280c8e93b75eb9fd1434ea177e6ab654

---
+++ release/assets/api-types-27a6e86d.js
@@ -0,0 +1,2 @@
+import{R as E,G as _}from"./apihub-builder.es-6ece7602.js";import{i as a}from"./arrays-b0979955.js";const t=E,A=_,I=[t,A],r={[t]:"REST API",[A]:"GraphQL API"};function c(P){return a(P)?Object.fromEntries(P.map(s=>[s.apiType,s])):void 0}export{I as A,r as a,t as b,A as c,c as t};
+//# sourceMappingURL=api-types-27a6e86d.js.map
