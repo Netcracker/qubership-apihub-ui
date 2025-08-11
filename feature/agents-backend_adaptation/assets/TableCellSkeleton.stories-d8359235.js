b22b8261608d92e591de13d8769d64e2636ee470
karpov-aleksandr <karpov-aleksandr@users.noreply.github.com>
2025-08-11 11:16:23 +0000

    deploy: 1b7247a6013b1c817671d42ac89ba1983cc7c69a

---
+++ feature/agents-backend_adaptation/assets/TableCellSkeleton.stories-d8359235.js
@@ -0,0 +1,9 @@
+import{j as e}from"./createTheme-e504d7a1.js";import{T as c}from"./TableCellSkeleton-8dbe868e.js";import{T as l,a as d}from"./TableRow-1448b6e7.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./Skeleton-bf6ca5e0.js";import"./clsx.m-e8952646.js";import"./index-65a2139a.js";import"./useThemeProps-be56d1e2.js";import"./emotion-react.browser.esm-6b7ec201.js";import"./hoist-non-react-statics.cjs-b1c88361.js";const k={title:"Table Cell Skeleton",component:c,decorators:[p=>e(l,{sx:{width:"900px"},children:e(d,{children:e(p,{})})})]},r={name:"Default"},t={name:"With maxWidth",args:{maxWidth:200}};var a,o,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: 'Default'
+}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};var s,i,n;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: 'With maxWidth',
+  args: {
+    maxWidth: 200
+  }
+}`,...(n=(i=t.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const w=["DefaultStory","MaxWidthStory"];export{r as DefaultStory,t as MaxWidthStory,w as __namedExportsOrder,k as default};
+//# sourceMappingURL=TableCellSkeleton.stories-d8359235.js.map
