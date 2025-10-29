1da9eae7bf59731647a2e05d2f3cc6e504d99c8a
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-10-29 23:54:59 +0400

    chore: Temporary fix for API prefix

--- packages/portal/src/routes/root/PortalPage/VersionPage/VersionAiAgentSubPage/api/REMOVE_IT.ts
+++ packages/portal/src/routes/root/PortalPage/VersionPage/VersionAiAgentSubPage/api/REMOVE_IT.ts
@@ -1,1 +1,4 @@
-export const STUB_API_PATH = '/stub/api/v1'
+import { API_V1 } from '@netcracker/qubership-apihub-ui-shared/utils/requests'
+
+export const STUB_API_PATH = `/api-linter${API_V1}`
+// export const STUB_API_PATH = `/stub${API_V1}`
