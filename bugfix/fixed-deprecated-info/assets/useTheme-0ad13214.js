f464185a2ec9e09401b5c733af8a02904f55206a
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2025-11-25 14:39:53 +0000

    deploy: c94a0536495c9418fbdf56a908fbf6486f87f3cb

---
+++ bugfix/fixed-deprecated-info/assets/useTheme-0ad13214.js
@@ -0,0 +1,2 @@
+import"./index-37ba2b57.js";import{d as e}from"./useThemeProps-90078124.js";import{u as m}from"./createTheme-d4c6d141.js";function s(){return m(e)}export{s as u};
+//# sourceMappingURL=useTheme-0ad13214.js.map
