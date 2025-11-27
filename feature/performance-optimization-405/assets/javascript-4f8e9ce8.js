b81ef08b3df4794d4b5ef7da5573da7d58dc5fa9
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2025-11-27 15:11:56 +0000

    deploy: e953cd28a8daeef0b48ce8c9fd2bf87b72dfc6b9

--- feature/performance-optimization-405/assets/javascript-4f8e9ce8.js
+++
@@ -1,7 +0,0 @@
-import{conf as t,language as e}from"./typescript-10954d26.js";import"./toggleHighContrast-2a3a887e.js";import"./iframe-44ff92f3.js";import"../sb-preview/runtime.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var a=t,n={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{a as conf,n as language};
-//# sourceMappingURL=javascript-4f8e9ce8.js.map
