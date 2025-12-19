479de34d59a32af9a03739f547240c766cc8e4eb
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2025-12-19 17:01:56 +0000

    deploy: 51a6e9dc49736cff58b971b82740e4eb8154f075

---
+++ feature/asyncapi-document-page/assets/api-types-9510ed10.js
@@ -0,0 +1,2 @@
+import{R as E,G as I,A as a}from"./apihub-builder.es-95465d40.js";import{i as T}from"./arrays-b0979955.js";const s=E,_=I,t=a,c=[s,_,t],n={[s]:"REST API",[_]:"GraphQL API",[t]:"AsyncAPI"};function r(P){return T(P)?Object.fromEntries(P.map(A=>[A.apiType,A])):void 0}export{c as A,n as a,s as b,_ as c,t as d,r as t};
+//# sourceMappingURL=api-types-9510ed10.js.map
