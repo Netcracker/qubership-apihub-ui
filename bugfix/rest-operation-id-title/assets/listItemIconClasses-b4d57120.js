ce484b786940ba4add50f85ab60ec67c74941e71
makeev-pavel <makeev-pavel@users.noreply.github.com>
2026-01-27 12:03:20 +0000

    deploy: dda8dd5fa214fdd9e0c08acdb020037a3bec284e

---
+++ bugfix/rest-operation-id-title/assets/listItemIconClasses-b4d57120.js
@@ -0,0 +1,2 @@
+import{g as s,b as e}from"./clsx.m-6cf3b989.js";function l(t){return e("MuiListItemIcon",t)}const i=s("MuiListItemIcon",["root","alignItemsFlexStart"]),n=i;export{l as g,n as l};
+//# sourceMappingURL=listItemIconClasses-b4d57120.js.map
---
+++ bugfix/rest-operation-id-title/assets/listItemTextClasses-450da6a6.js
@@ -0,0 +1,2 @@
+import{g as e,b as s}from"./clsx.m-6cf3b989.js";function l(t){return s("MuiListItemText",t)}const i=e("MuiListItemText",["root","multiline","dense","inset","primary","secondary"]),n=i;export{l as g,n as l};
+//# sourceMappingURL=listItemTextClasses-450da6a6.js.map
