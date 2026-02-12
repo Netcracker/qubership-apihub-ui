cbdb4d2c2c01897c6a7ac4d8d6a7ef6c2d6fea73
JayLim2 <JayLim2@users.noreply.github.com>
2026-02-12 06:26:13 +0000

    deploy: 8562cb65b3c5fa7b2b87e97df2d11c0407f55db9

---
+++ bugfix/no-internal-doc-error/assets/ExpandableItem.stories-f8b9bbf3.js
@@ -0,0 +1,11 @@
+import{j as m}from"./createTheme-d4c6d141.js";import{R as p}from"./placeholder-colors-743b01e8.js";import{E as a}from"./ExpandableItem-71c317e5.js";import{B as n}from"./Box-11e4398c.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./KeyboardArrowDownOutlined-29dbf4ff.js";import"./jsx-runtime_commonjs-proxy-74302a61.js";import"./createSvgIcon-473ccb66.js";import"./clsx.m-6cf3b989.js";import"./ClassNameGenerator-bd600f10.js";import"./useThemeProps-90078124.js";import"./createChainedFunction-0bab83cf.js";import"./ownerDocument-613eb639.js";import"./ownerWindow-03d1c82d.js";import"./TransitionGroupContext-f40e0036.js";import"./useEnhancedEffect-9d60ea74.js";import"./debounce-517eeb3c.js";import"./isMuiElement-de695f11.js";import"./useId-f6e37502.js";import"./useControlled-1dfdd739.js";import"./IconButton-c2813217.js";import"./ButtonBase-1b5192e9.js";import"./emotion-react.browser.esm-f969ea94.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-5494d9cc.js";import"./extendSxProp-acc1143e.js";const q={title:"Expandable Item",component:a},o={name:"Default",args:{children:m(n,{sx:{backgroundColor:p},children:"An Expandable Item Component"}),showToggler:!0,expanded:!1}};var r,t,e;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Default',
+  args: {
+    children: <Box sx={{
+      backgroundColor: RED_SECTION_COLOR
+    }}>An Expandable Item Component</Box>,
+    showToggler: true,
+    expanded: false
+  }
+}`,...(e=(t=o.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const v=["DefaultStory"];export{o as DefaultStory,v as __namedExportsOrder,q as default};
+//# sourceMappingURL=ExpandableItem.stories-f8b9bbf3.js.map
