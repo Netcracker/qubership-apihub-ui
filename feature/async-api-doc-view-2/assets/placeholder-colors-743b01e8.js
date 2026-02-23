2af4ca23b34be114d9351339731fda18f7705202
vOrigins <vOrigins@users.noreply.github.com>
2026-02-23 15:57:36 +0000

    deploy: 358d9b1bcaccc8fb17e777ad3b371f9ee279818e

---
+++ feature/async-api-doc-view-2/assets/placeholder-colors-743b01e8.js
@@ -0,0 +1,2 @@
+const O="#E56E94",E="#C2E5D3",C="#ADD8E6",s="#cb7ede",R="#ffc261",_="#edac7e";export{C as B,E as G,R as O,s as P,O as R,_ as a};
+//# sourceMappingURL=placeholder-colors-743b01e8.js.map
