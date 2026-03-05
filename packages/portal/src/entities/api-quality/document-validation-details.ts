4d5ef1e45fb9ade13bd1f8193b37e76e38cb7652
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-03-05 10:53:18 +0400

    fix: Fix build

--- packages/portal/src/entities/api-quality/document-validation-details.ts
+++ packages/portal/src/entities/api-quality/document-validation-details.ts
@@ -1,9 +1,10 @@
 import type { Issue, IssueDto } from './issues'
-import type { RulesetLinter, RulesetMetadata, RulesetMetadataDto } from './rulesets'
+import type { LinterDto } from './linters'
+import type { RulesetMetadata, RulesetMetadataDto } from './rulesets'
 import type { ValidatedDocument, ValidatedDocumentDto } from './validated-documents'
 
 type ValidationResultDto = {
-  linter: RulesetLinter
+  linter: LinterDto['linter']
   ruleset: RulesetMetadataDto
   issues: readonly IssueDto[]
 }
