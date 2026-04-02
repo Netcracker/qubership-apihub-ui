1cb5f883d3e636222983ce98acdd771cde4c1ed2
b41ex <b41ex@users.noreply.github.com>
2026-04-02 19:23:39 +0000

    deploy: acb834322a9dd8a2c9325eed980c431d624d7b98

--- develop/assets/TableCellSkeleton.stories-bf24488c.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./createTheme-d4c6d141.js";import{T as c}from"./TableCellSkeleton-c35561e0.js";import{T as l,a as d}from"./TableRow-9ee80158.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./Skeleton-e0fe84cc.js";import"./clsx.m-6cf3b989.js";import"./ClassNameGenerator-bd600f10.js";import"./useThemeProps-90078124.js";import"./emotion-react.browser.esm-f969ea94.js";const j={title:"Table Cell Skeleton",component:c,decorators:[p=>t(l,{sx:{width:"900px"},children:t(d,{children:t(p,{})})})]},r={name:"Default"},e={name:"With maxWidth",args:{maxWidth:200}};var a,o,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  name: 'Default'
-}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};var s,i,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  name: 'With maxWidth',
-  args: {
-    maxWidth: 200
-  }
-}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const k=["DefaultStory","MaxWidthStory"];export{r as DefaultStory,e as MaxWidthStory,k as __namedExportsOrder,j as default};
-//# sourceMappingURL=TableCellSkeleton.stories-bf24488c.js.map
