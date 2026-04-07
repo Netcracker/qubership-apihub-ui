37f860549ed3bc244ff77bafe5e3dad9670903bb
vOrigins <vOrigins@users.noreply.github.com>
2026-04-07 09:40:46 +0000

    deploy: 9db55fdf3366b068fdd8c18fe3871bc232cc0f1b

---
+++ bugfix/dashboard-linter-summary/assets/TableCellSkeleton.stories-53e02a4c.js
@@ -0,0 +1,9 @@
+import{j as t}from"./createTheme-877270e5.js";import{T as c}from"./TableCellSkeleton-840561dd.js";import{T as l,a as d}from"./TableRow-b8fd4ed6.js";import"./index-37ba2b57.js";import"./_commonjsHelpers-de833af9.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./Skeleton-da25cf81.js";import"./clsx.m-30fbb483.js";import"./ClassNameGenerator-bd600f10.js";import"./useThemeProps-f3b34960.js";import"./emotion-react.browser.esm-39a29fa1.js";const j={title:"Table Cell Skeleton",component:c,decorators:[p=>t(l,{sx:{width:"900px"},children:t(d,{children:t(p,{})})})]},r={name:"Default"},e={name:"With maxWidth",args:{maxWidth:200}};var a,o,m;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: 'Default'
+}`,...(m=(o=r.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};var s,i,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  name: 'With maxWidth',
+  args: {
+    maxWidth: 200
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const k=["DefaultStory","MaxWidthStory"];export{r as DefaultStory,e as MaxWidthStory,k as __namedExportsOrder,j as default};
+//# sourceMappingURL=TableCellSkeleton.stories-53e02a4c.js.map
