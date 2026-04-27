ef393b132041a2528c8ebe90492f3f8a2ba71e79
Vladyslav Novikov <vladyslav.novikov@netcracker.com>
2026-04-27 13:40:58 +0300

    feat(icons): add NewChatIcon and SendPlaneIcon SVG components

---
+++ packages/shared/src/icons/SendPlaneIcon.tsx
@@ -0,0 +1,15 @@
+import { createSvgIcon } from '@mui/material'
+
+export const SendPlaneIcon = createSvgIcon(
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_2837_17605)">
+      <path d="M2.83332 16.9999L17.375 10.7666C18.05 10.4749 18.05 9.52493 17.375 9.23327L2.83332 2.99993C2.28332 2.75827 1.67499 3.1666 1.67499 3.75827L1.66666 7.59993C1.66666 8.0166 1.97499 8.37493 2.39166 8.42493L14.1667 9.99993L2.39166 11.5666C1.97499 11.6249 1.66666 11.9833 1.66666 12.3999L1.67499 16.2416C1.67499 16.8333 2.28332 17.2416 2.83332 16.9999Z" />
+    </g>
+    <defs>
+      <clipPath id="clip0_2837_17605">
+        <rect width="20" height="20" />
+      </clipPath>
+    </defs>
+  </svg>,
+  'SendPlane',
+)
