9c95188076876e101ed790817ed26968d5d3edf4
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2026-05-14 07:39:00 +0000

    deploy: c435b16d2f952280952d95e7c3473590fd924d5c

---
+++ bugfix/rulesets-instead-of-the-active-one-overview-page/assets/GlobalStyles-eed42537.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./index-37ba2b57.js";import{d as e}from"./useThemeProps-f3b34960.js";import{j as r}from"./createTheme-877270e5.js";import{G as m}from"./GlobalStyles-1202dfb7.js";function p(o){return r(m,t({},o,{defaultTheme:e}))}export{p as G};
+//# sourceMappingURL=GlobalStyles-eed42537.js.map
