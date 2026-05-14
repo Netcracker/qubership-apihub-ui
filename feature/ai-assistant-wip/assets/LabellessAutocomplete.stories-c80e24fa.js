a64103c0574ff24f7da1c08e0cc33b4c705b38ba
vOrigins <vOrigins@users.noreply.github.com>
2026-05-14 19:11:06 +0000

    deploy: 0a44e02dbd2a61ab9ccf2394a525dbb1aabcc829

---
+++ feature/ai-assistant-wip/assets/LabellessAutocomplete.stories-c80e24fa.js
@@ -0,0 +1,23 @@
+import{j as t}from"./createTheme-877270e5.js";import{r as u}from"./index-37ba2b57.js";import{T as A,t as S}from"./theme-b2b6f141.js";import{A as _}from"./Autocomplete-2dc83aaf.js";import{B as v}from"./Box-875ed2fd.js";import{T as f}from"./TextField-769f4af4.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-9c75dc50.js";import"./_commonjsHelpers-de833af9.js";import"./useId-f6e37502.js";import"./useEnhancedEffect-9d60ea74.js";import"./GlobalStyles-1202dfb7.js";import"./emotion-react.browser.esm-39a29fa1.js";import"./colors-f8087473.js";import"./palette-f80b486f.js";import"./clsx.m-30fbb483.js";import"./ClassNameGenerator-bd600f10.js";import"./useThemeProps-f3b34960.js";import"./Close-192a2a3f.js";import"./createSvgIcon-bf6c1b92.js";import"./IconButton-457c30a1.js";import"./ButtonBase-db27d07e.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-5494d9cc.js";import"./TransitionGroupContext-f40e0036.js";import"./Popper-c373d65a.js";import"./ownerDocument-613eb639.js";import"./Portal-2ef22c9d.js";import"./isHostComponent-73d6e646.js";import"./index-4da2af8c.js";import"./Paper-3bbc0c25.js";import"./useControlled-1dfdd739.js";import"./usePreviousProps-c4e5492e.js";import"./Chip-5383660e.js";import"./extendSxProp-e9808817.js";import"./FormLabel-a9631dce.js";import"./isMuiElement-de695f11.js";import"./index-891d46e9.js";import"./Menu-95baf0e4.js";import"./useTheme-951578b7.js";import"./Modal-0f0476c1.js";import"./utils-c5c5ff4b.js";import"./ownerWindow-03d1c82d.js";import"./createChainedFunction-0bab83cf.js";import"./debounce-517eeb3c.js";import"./Grow-676211cf.js";import"./List-8dec22e9.js";import"./ListContext-aeaca2aa.js";import"./GlobalStyles-eed42537.js";function b({maxWidth:e,...p}){return t(_,{sx:{"& .MuiAutocomplete-inputRoot":{py:1,maxWidth:{maxWidth:e},"&.MuiInputBase-sizeSmall":{py:1}},"& .MuiAutocomplete-tag":{my:.25,ml:0,mr:1}},...p})}b.displayName="LabellessAutocomplete";const i=u.memo(b);try{i.displayName="LabellessAutocomplete",i.__docgenInfo={description:`A specialized Autocomplete component without a visible label.
+Provides consistent styling for all autocomplete inputs across the application.
+Particularly useful for forms where label space needs to be conserved.`,displayName:"LabellessAutocomplete",props:{maxWidth:{defaultValue:null,description:"",name:"maxWidth",required:!1,type:{name:"MaxWidth<string | number>"}},ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"Ref<unknown>"}}}}}catch{}const h=["Option 1","Option 2","Option 3","Option 4","Option 5"],ye={title:"Autocompletes/Labelless Autocomplete",component:i,parameters:{layout:"centered"},decorators:[e=>t(A,{theme:S,children:t(v,{sx:{width:"600px",p:2},children:t(e,{})})})],argTypes:{options:{control:"object",description:"Array of options to display in the dropdown"},freeSolo:{control:"boolean",description:"Allow free text input that is not in the options list"},disabled:{control:"boolean",description:"Disable the component"},maxWidth:{control:{type:"number"},description:'Maximum width of the component (e.g., "300px", "50%")'},value:{control:"object",description:"Initial selected value(s)"},multiple:{control:"boolean",table:{disable:!0},description:"Allow multiple selections"},renderInput:{table:{disable:!0},description:"Render the input. Used to customize the TextField."},ref:{table:{disable:!0}}}},x=e=>{const[p,g]=u.useState(e.value||[]);return t(i,{...e,value:p,onChange:(n,y)=>{g(y)},getOptionLabel:n=>String(n)})},o={args:{options:h,multiple:!1,freeSolo:!1,disabled:!1,renderInput:e=>t(f,{...e})},render:x},r={args:{options:h,multiple:!0,freeSolo:!1,disabled:!1,value:["Option 1"],renderInput:e=>t(f,{...e})},render:x};var l,a,s;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    options: options,
+    multiple: false,
+    freeSolo: false,
+    disabled: false,
+    renderInput: params => <TextField {...params} />
+  },
+  render: RenderWithState
+}`,...(s=(a=o.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};var m,d,c;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    options: options,
+    multiple: true,
+    freeSolo: false,
+    disabled: false,
+    value: ['Option 1'],
+    renderInput: params => <TextField {...params} />
+  },
+  render: RenderWithState
+}`,...(c=(d=r.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const Ae=["Default","MultipleSelection"];export{o as Default,r as MultipleSelection,Ae as __namedExportsOrder,ye as default};
+//# sourceMappingURL=LabellessAutocomplete.stories-c80e24fa.js.map
