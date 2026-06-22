3a24332415b998ae8a15fd419cbdbf2a2be63106
vOrigins <vOrigins@users.noreply.github.com>
2026-06-22 10:03:49 +0000

    deploy: 6be76f0cc0e15156ba4a90c19272c9699078797f

---
+++ feature/contracts-shared-utils/assets/javascript-3292682f.js
@@ -0,0 +1,7 @@
+import{conf as t,language as e}from"./typescript-e68f9128.js";import"./toggleHighContrast-2f259558.js";import"./iframe-bace7af4.js";import"../sb-preview/runtime.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.52.2(404545bded1df6ffa41ea0af4e8ddb219018c6c1)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var a=t,n={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{a as conf,n as language};
+//# sourceMappingURL=javascript-3292682f.js.map
