337b7e08dabe45e3e0f46b8b08169e6a0e599b55
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2026-06-26 19:14:22 +0400

    feat: simple view

--- packages/portal/src/routes/root/PortalPage/VersionPage/VersionContractsSubPage/DdlTableContentView.tsx
+++ packages/portal/src/routes/root/PortalPage/VersionPage/VersionContractsSubPage/DdlTableContentView.tsx
@@ -1,10 +1,10 @@
-import { DocSpecView } from '@apihub/components/DocSpecView'
 import { buildFromDdlInBrowser } from '@apihub/utils/buildFromDdlInBrowser'
 import { Box } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import type { Realm } from '@netcracker/qubership-apihub-ddlapi'
 import { type FC, memo, useCallback, useEffect, useMemo, useState } from 'react'
 
+import { DdlTableViewer } from '@netcracker/qubership-apihub-api-doc-viewer'
 import { RawSpecView } from '@netcracker/qubership-apihub-ui-shared/components/SpecificationDialog/RawSpecView'
 import type { SpecViewMode } from '@netcracker/qubership-apihub-ui-shared/components/SpecViewToggler'
 import {
@@ -13,12 +13,13 @@ import {
   SIMPLE_SPEC_VIEW_MODE,
 } from '@netcracker/qubership-apihub-ui-shared/components/SpecViewToggler'
 import {
+  DETAILED_SCHEMA_VIEW_MODE,
   SIMPLE_SCHEMA_VIEW_MODE,
 } from '@netcracker/qubership-apihub-ui-shared/entities/schema-view-mode'
-import { SQL_FILE_EXTENSION, SQL_FILE_FORMAT } from '@netcracker/qubership-apihub-ui-shared/utils/files'
+import { theme } from '@netcracker/qubership-apihub-ui-shared/themes/theme'
+import { SQL_FILE_EXTENSION } from '@netcracker/qubership-apihub-ui-shared/utils/files'
 import { DDL_DOCUMENT_TYPE } from '@netcracker/qubership-apihub-ui-shared/utils/specs'
 import { toFormattedJsonString } from '@netcracker/qubership-apihub-ui-shared/utils/strings'
-import { DdlTableViewer } from '@netcracker/qubership-apihub-api-doc-viewer'
 
 export type DdlTableContentViewProps = {
   data: string | Record<string, unknown> | undefined
@@ -108,24 +109,27 @@ export const DdlTableContentView: FC<DdlTableContentViewProps> = memo<DdlTableCo
         parseError
           ? <ParseErrorMessage>{parseError}</ParseErrorMessage>
           : (
-            <Box px={2} py={2}>
-              <DdlTableViewer
-                source={normalizedSource}
-                tableKey={tableKey}
-                navigationCallback={navigationCallback}
-                devMode={true}
-              />
-            </Box>
+            <DdlTableViewer
+              source={normalizedSource}
+              tableKey={tableKey}
+              navigationCallback={navigationCallback}
+              displayMode={DETAILED_SCHEMA_VIEW_MODE}
+            />
           )
       )}
 
       {viewMode === SIMPLE_SPEC_VIEW_MODE && (
-        <DocSpecView
-          value={content}
-          type={DDL_DOCUMENT_TYPE.DDL}
-          format={SQL_FILE_FORMAT}
-          schemaViewMode={SIMPLE_SCHEMA_VIEW_MODE}
-        />
+        parseError
+          ? <ParseErrorMessage>{parseError}</ParseErrorMessage>
+          : (
+            <DdlTableViewer
+              source={normalizedSource}
+              tableKey={tableKey}
+              navigationCallback={navigationCallback}
+              displayMode={SIMPLE_SCHEMA_VIEW_MODE}
+              devMode={true}
+            />
+          )
       )}
 
       {viewMode === RAW_SPEC_VIEW_MODE && (
@@ -146,6 +150,7 @@ const ContentContainer = styled(Box)({
   flexDirection: 'column',
   height: '100%',
   overflow: 'hidden',
+  padding: theme.spacing(2),
 })
 
 const ParseErrorMessage = styled(Box)(({ theme }) => ({
