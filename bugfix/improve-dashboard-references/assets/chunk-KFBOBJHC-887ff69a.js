984392c7d3f44c59b68a071c54131b3c42a081bd
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2026-07-20 14:47:21 +0000

    deploy: 9655b4beea8684c2f9c5491e9c06b9c317f5c34b

---
+++ bugfix/improve-dashboard-references/assets/chunk-KFBOBJHC-887ff69a.js
@@ -0,0 +1,2 @@
+import{_ as s}from"./mermaid.core-3bd4d4f4.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
+//# sourceMappingURL=chunk-KFBOBJHC-887ff69a.js.map
