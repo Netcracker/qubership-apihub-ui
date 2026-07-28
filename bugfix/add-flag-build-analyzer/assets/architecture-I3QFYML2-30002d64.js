267fe37b52712e1c263e841ecd547454d6a9d900
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2026-07-28 07:11:41 +0000

    deploy: 731d698a08c26a280d2e2a047476d2bf905e8c38

---
+++ bugfix/add-flag-build-analyzer/assets/architecture-I3QFYML2-30002d64.js
@@ -0,0 +1,2 @@
+import{A as f,e as l}from"./mermaid-parser.core-ab599d10.js";import"./iframe-a33fcf79.js";import"../sb-preview/runtime.js";import"./mermaid.core-618f8185.js";import"./_commonjsHelpers-de833af9.js";import"./_Set-c5d192bc.js";import"./index-356e4a49.js";import"./reduce-d67e21a4.js";import"./_baseUniq-2773af2d.js";import"./_basePickBy-a247c4a5.js";import"./uniq-9f9a8bf7.js";import"./clone-ccf1cc38.js";import"./main-4b30b16b.js";export{f as ArchitectureModule,l as createArchitectureServices};
+//# sourceMappingURL=architecture-I3QFYML2-30002d64.js.map
