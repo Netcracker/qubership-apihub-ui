2a4421f997e842e068df6fc0b9d45a94df5c71da
iurii-golovinskii <iurii-golovinskii@users.noreply.github.com>
2026-09-04 07:39:12 +0000

    deploy: 8ed4fedf5b328ca06c17c42564c265e75ced2cba

---
+++ bugfix/global-search-documents-packages-does-not-consider-api-type-589/assets/chunk-KFBOBJHC-1f2da6e4.js
@@ -0,0 +1,2 @@
+import{_ as s}from"./mermaid.core-ef48143d.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
+//# sourceMappingURL=chunk-KFBOBJHC-1f2da6e4.js.map
